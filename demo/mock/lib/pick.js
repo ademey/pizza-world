a21818080b8485e40e8cf348afe53c0373150a1f
Anne Demey <anneyvdemey@gmail.com>
2019-01-07 08:30:05 -0600

    Starting pizza generator

---
+++ demo/mock/lib/pick.js
@@ -0,0 +1,13 @@
+import { getRandom } from './util'
+
+/**
+ * Create a function that returns a random item from an array.
+ * @type {ProviderCreator}
+ * @param {array} list - Array to seed the provider
+ * @return {function(): any}
+ * @example
+ * const randomFruit = pick(['Apple', 'Banana', 'Cherry']);
+ * randomFruit() // 'Banana'
+ * randomFruit() // 'Apple'
+ */
+export const pick = list => () => getRandom(list)
--- demo/src/views/Store/Store.js
+++ demo/src/views/Store/Store.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Menu } from "views/Menu";
 import StoreHeader from "./StoreHeader";
+import './store.css';
 
 const Store = () => (
   <div className="pw-store-view">
